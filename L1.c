4cfe877d89063552993e636462168a8da1837131
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-02-02 15:02:06 +0530

    Update L1.c

--- L1.c
+++ L1.c
@@ -4,25 +4,26 @@ void main()
 int a[20],n,i,j,temp;
 printf("\nEnter the numbers");
 scanf("%d",&n);
-printf("\nArray values");
+printf("\n the Array values");
+scanf("%d",&a[20]);
 for(i=0;i<n;i++)
 {
 	scanf("\n%d",&a[i]);
 }
 for(i=0;i<n;i++)
 {
-	for(j=i+1;j<n;j++)
-	{
-		if(a[i]<a[j])
-		{
-			temp=a[i];
-			a[i]=a[j];
-			a[j]=temp;
-		}
-	}
+for(j=i+1;j<n;j++)
+{
+if(a[i]<a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
 }
-    for(i=0;i<n;i++)
-    {
-	printf("%d",a[i]);
-    }
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
 }
