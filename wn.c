1069fd9ffe5c8ff746d1abe4e9915ce92e623a3c
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-03-01 10:27:52 +0000

    Create wn.c

---
+++ wn.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\n enter the number:");
+scanf("%d",&n);
+switch(n)
+{
+case 1:
+printf("One");
+break;
+case 2:
+printf("Two");
+break;
+case 3:
+printf("Three");
+break;
+case 4:
+printf("Four");
+break;
+case 5:
+printf("Five");
+break;
+case 6:
+printf("Six");
+break;
+case 7:
+printf("Seven");
+break;
+case 8:
+printf("Eight");
+break;
+case 9:
+printf("Nine");
+break;
+case 10:
+printf("ten");
+break;
+default:
+printf("1-10 only)");
+break;
+}
+return 0;
+}
