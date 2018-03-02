2191555e00793c424b8ff0e060ee3e9e073ca451
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-03-02 14:51:01 +0530

    Create ma.c

---
+++ ma.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i,max,a[50];
+	max=a[0];
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		if(a[i]>max)
+		{
+			max=a[i];
+		}
+	}
+	printf("%d",max);
+	return 0;
+}
