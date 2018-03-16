30c7ea5a795eaed96e5c8260858ced66265d4eaf
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-03-16 14:54:45 +0530

    Create sqr.c

---
+++ sqr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int a,b,c;
+    printf("enter the number:");
+    scanf("%d%d",&a,&b);
+    c=a*b;
+if(a/b==1)
+{
+    printf("Yes");
+}
+else
+{
+    printf("No");
+}
+return 0;
+}
