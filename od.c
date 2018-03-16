7061d938d90af70db4c8bc891a8937d688fc44d3
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-03-16 15:09:21 +0530

    Create od.c

---
+++ od.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int n,t,r=0,i;
+	printf("enter the number:");
+    scanf("%d",&n);
+	while(n!=0)
+	{
+		t=n%10;
+		r=(r*10)+t;
+		n=n/10;
+	}
+	while(r!=0)
+	{
+		i=r%10;
+		r=r/10;
+		if(i%2!=0)
+		{
+		  printf("%d\n",i);
+		}
+			
+	}
+  return 0;
+}
