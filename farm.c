4cf2f3e3c4cd60e0dea069a55826a4e9008df1b8
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-03-20 16:13:10 +0530

    Create farm.c

---
+++ farm.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float l,b;
+printf("enter the length:");
+scanf("%f",&l);
+printf("enter the breath:");
+scanf("%f",&b);
+printf("\n AREA:%5f",(l*b));
+return 0;
+}
