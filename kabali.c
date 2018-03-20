68580a543be40d191f25c8c34da6710fa5e91856
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-03-20 15:58:20 +0530

    Create kabali.c

---
+++ kabali.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int x,y,d;
+printf("enter the number of ninjas in kabali's team:");
+scanf("%d",&x);
+printf("enter number of ninjas in opponent team:");
+scanf("%d",&y);
+d=x-y;
+printf("difference is %d\n",d);
+if(x>y)
+{
+printf("kabali went to fight");
+}
+else
+{
+printf("kabali did not went to fight");
+}
+return 0;
+}
